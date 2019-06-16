5733ff239c79bfe29b62001abe8ed15f4fb2bd34
Alexander Principle <46525581+aprinciple@users.noreply.github.com>
2019-06-16 11:11:24 +0300

    Исправлена ошибка typeof(null)
    typeof(null) - возвращает "object". "object" не может сравниваться с null;
    Если будет задача прекратить функции при "Cancel", который возвращает null;
    let answer = null;
    if(answer == null) { // typeof(answer) == null - не будет работать, потому что сравниваться будет "object" == null
      break;
    }
    
    В последующих задачах/файлах такой же код - нужно исправить.

--- Задание_3/script.js
+++ Задание_3/script.js
@@ -18,7 +18,7 @@ for (let i = 0; i < 2; i++) {
     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
         b = prompt ("Во сколько обойдется?", "");
 
-    if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+    if (typeof(a)=== 'string' && a != null && b != null && a != "" && b != "" && a.length < 50) {
 
         console.log ("done");
 
@@ -39,7 +39,7 @@ for (let i = 0; i < 2; i++) {
 //     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
 //         b = prompt ("Во сколько обойдется?", "");
 
-//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+//     if (typeof(a)=== 'string' && a != null && b != null && a != "" && b != "" && a.length < 50) {
 
 //         console.log ("done");
 
@@ -61,7 +61,7 @@ for (let i = 0; i < 2; i++) {
 //     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
 //         b = prompt ("Во сколько обойдется?", "");
 
-//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+//     if (typeof(a)=== 'string' && a != null && b != null && a != "" && b != "" && a.length < 50) {
 
 //         console.log ("done");
 
