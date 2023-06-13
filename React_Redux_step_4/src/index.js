9534c4c67977ba2eee5e4c92d8d390f6c1160a5b
Ivan Petrychenko <ivanpetrychenko95@gmail.com>
2023-06-13 14:58:08 +0100

    some lib versions were updated

--- React_Redux_step_4/src/index.js
+++ React_Redux_step_4/src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/app';
+import { createRoot } from "react-dom/client";
+import App from "./components/app/app";
 
-import './index.css';
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
